cd95129145dda660f6a4cb1b107dd4bbbe036860
PrashantThombre <thombreprash@gmail.com>
2018-11-12 23:59:42 -0800

    Adding string.reveseString

---
+++ src/string/Solution02ReverseInteger.java
@@ -0,0 +1,13 @@
+package string;
+
+public class Solution02ReverseInteger {
+	public int reverse(int x) {
+		long ans = 0;
+		while (x != 0) {
+			ans = ans * 10 + x % 10;
+			x /= 10;
+		}
+		return (ans < Integer.MIN_VALUE || ans > Integer.MAX_VALUE) ? 0
+				: (int) ans;
+	}
+}
